edaa33fee3865a1cf0e72b0e43a51eb21800d7da
BuildTools <unconfigured@null.spigotmc.org>
2024-09-17 18:46:55 +0800

    Initial commit

---
+++ src/main/java/net/crimson/additional/Additional.java
@@ -0,0 +1,16 @@
+package net.crimson.additional;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Additional implements ModInitializer {
+	public static final String MOD_ID = "additional";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/crimson/additional/AdditionalClient.java
@@ -0,0 +1,10 @@
+package net.crimson.additional;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdditionalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
